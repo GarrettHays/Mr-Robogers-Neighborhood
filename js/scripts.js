3bbaa206a6faa25f43e8af771b676ebb689d8b30
Garrett Hays <GarrettlHays@gmail.com>
2022-06-10 13:07:41 -0700

    rearrange heirarchy of if statements to field numbers 3, 2, then 1.

--- js/scripts.js
+++ js/scripts.js
@@ -5,35 +5,33 @@ function numberCounter(number) {
   for (let i = 0; i <= numberInt; i++) {
     newArray.push(i.toString());
   }
-  console.log(newArray);
   return newArray;
 }
 
 // Business Logic
 function roboger(number) {
   let phrase = [];
+  if (typeof number !== "string") {
+    return "Number must be in string format";
+  }
   if (number.trim().length === 0) {
-    return "Enter a Number";
+    return "Enter a Number"; 
   }
   let numberString = numberCounter(number); 
 
   for(let i = 0; i < numberString.length; i++) {
-    if (numberString[i].includes(1)) {
-      phrase.push("beep!");  
+    if (numberString[i].includes(3)) {
+      phrase.push("Won't you be my neighbor?"); 
     } else if (numberString[i].includes(2)) {
       phrase.push("boop!"); 
+    } else if (numberString[i].includes(1)) {
+      phrase.push("beep!");   
     } else {
       phrase.push("" + numberString[i]);
+      
   }
   };
   return phrase.toString();
 }
 
-// // UI Logic
-// $(document).ready(function(){
-//   $("form#phrase").submit(function(event){
-//     event.preventDefault();
 
-
-//   });
-// });
