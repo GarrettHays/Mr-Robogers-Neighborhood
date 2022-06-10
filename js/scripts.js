d010575fb9a42f87c9286e4bda8b77465b361188
Garrett Hays <GarrettlHays@gmail.com>
2022-06-10 09:04:40 -0700

    update business logic for js file

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// Business Logic
+function forLoop(number) {
+  let phrase = [];
+
+  for(var i = 1; i <= number; i++) {
+    if (number.includes("1"[i])) {
+      phrase.push("beep");
+    } else if (number.includes("2"[i])) {
+      phrase.push("boop");
+    } (number.includes("3"[i])) {
+        phrase.push("Won't you be my neighbor?");
+    } else {
+      result.push(i);
+    }
+  }
+
+  return result;
+
+};
+
+// UI Logic
